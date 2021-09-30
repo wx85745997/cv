2ae20c3281ecb97c3758d4886654682b48ae5d3a
wx85745997 <497045233@qq.com>
2021-09-30 17:58:07 +0800

    1
    Signed-off-by: wx85745997 <497045233@qq.com>

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -3,23 +3,48 @@ import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
 
-const fakeUsersService: Partial<UsersService> = {
-  find: () => Promise.resolve([]),
-  create: (email: string, password: string) =>
-    Promise.resolve({ id: 1, email, password } as User),
-};
+let service: AuthService;
+let fakeUsersService: Partial<UsersService>;
 
-it('can create an instance of auth service', async () => {
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      {
-        provide: UsersService,
-        useValue: fakeUsersService,
-      },
-    ],
-  }).compile();
+describe('AuthService', () => {
+  beforeEach(async () => {
+    fakeUsersService = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
+    };
 
-  const service = module.get(AuthService);
-  expect(service).toBeDefined();
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
+    }).compile();
+
+    service = module.get(AuthService);
+  });
+
+  it('can create an instance of auth service', async () => {
+    expect(service).toBeDefined();
+  });
+  it('creates a new user with a salted and hashed password', async () => {
+    const user = await service.signup('123@qq.com', '123');
+    expect(user.password).not.toEqual('123');
+    const [salt, hash] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
+
+  it('throws an error if user signs up with email that is in use', async (done) => {
+    fakeUsersService.find = () =>
+      Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+    try {
+      await service.signup('123@qq.com', '123');
+    } catch (err) {
+      done();
+    }
+  });
 });
